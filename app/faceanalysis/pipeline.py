d806a7a18763209552cc34b3fbd1644dc59b402b
Clemens Wolff <clewolff@microsoft.com>
2018-08-08 10:53:09 -0400

    Remove unnecessary Pipeline class

--- app/faceanalysis/pipeline.py
+++ app/faceanalysis/pipeline.py
@@ -1,5 +1,3 @@
-# pylint: disable=too-few-public-methods
-
 import json
 import os
 
@@ -20,179 +18,173 @@ from faceanalysis.settings import FACE_VECTORIZE_ALGORITHM
 from faceanalysis.settings import IMAGE_PROCESSOR_QUEUE
 
 
-class Pipeline:
-    def __init__(self):
-        self.db = get_database_manager()
-        self.logger = get_logger(__name__)
-        dirname = os.path.dirname(os.path.abspath(__file__))
-        self.img_dir = os.path.join(dirname, 'images')
-        self.logger.debug('pipeline initialized')
+db = get_database_manager()
+logger = get_logger(__name__)
+img_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'images')
 
-    def _add_entry_to_session(self, cls, session, **kwargs):
-        self.logger.debug('adding entry to session')
-        row = cls(**kwargs)
-        session.add(row)
-        return row
 
-    def _find_image(self, img_id, session):
-        self.logger.debug('finding image %s', img_id)
+def _add_entry_to_session(cls, session, **kwargs):
+    logger.debug('adding entry to session')
+    row = cls(**kwargs)
+    session.add(row)
+    return row
 
-        img_path = None
-        for extension in ALLOWED_EXTENSIONS:
-            img_name = "{}.{}".format(img_id, extension)
-            fpath = os.path.join(self.img_dir, img_name)
-            if os.path.isfile(fpath):
-                img_path = fpath
-                break
 
-        if img_path:
-            self._add_entry_to_session(Image,
-                                       session,
-                                       img_id=img_id)
+def _find_image(img_id, session):
+    logger.debug('finding image %s', img_id)
 
-        return img_path
+    img_path = None
+    for extension in ALLOWED_EXTENSIONS:
+        img_name = "{}.{}".format(img_id, extension)
+        fpath = os.path.join(img_dir, img_name)
+        if os.path.isfile(fpath):
+            img_path = fpath
+            break
 
-    # pylint: disable=broad-except
-    def _delete_img(self, img_id):
-        self.logger.debug('deleting img')
-        for extension in ALLOWED_EXTENSIONS:
-            img_name = "{}.{}".format(img_id, extension)
-            fpath = os.path.join(self.img_dir, img_name)
-            try:
-                os.remove(fpath)
-                self.logger.debug("removed %s", img_id)
-            except Exception:
-                continue
+    if img_path:
+        _add_entry_to_session(Image, session, img_id=img_id)
 
-    def _process_feature_mapping(self, features, img_id, session):
-        self.logger.debug('processing feature mapping')
-        feature_str = json.dumps(features)
-        self._add_entry_to_session(FeatureMapping,
-                                   session,
-                                   img_id=img_id,
-                                   features=feature_str)
-        return features
+    return img_path
 
-    def _process_matches(self, this_img_id,
-                         that_img_id, distance_score, session):
-        self.logger.debug('processing matches')
-        self._add_entry_to_session(Match,
-                                   session,
-                                   this_img_id=this_img_id,
-                                   that_img_id=that_img_id,
-                                   distance_score=distance_score)
-        self._add_entry_to_session(Match,
-                                   session,
-                                   this_img_id=that_img_id,
-                                   that_img_id=this_img_id,
-                                   distance_score=distance_score)
 
-    def _get_img_ids_and_features(self):
-        self.logger.debug('getting all img ids and respective features')
-        session = self.db.get_session()
-        known_features = []
-        rows = session.query(FeatureMapping).all()
-        session.close()
-        img_ids = []
-        for row in rows:
-            img_ids.append(row.img_id)
-            current_features = np.array(json.loads(row.features))
-            known_features.append(current_features)
-        return img_ids, np.array(known_features)
+# pylint: disable=broad-except
+def _delete_img(img_id):
+    logger.debug('deleting img')
+    for extension in ALLOWED_EXTENSIONS:
+        img_name = "{}.{}".format(img_id, extension)
+        fpath = os.path.join(img_dir, img_name)
+        try:
+            os.remove(fpath)
+            logger.debug("removed %s", img_id)
+        except Exception:
+            continue
+# pylint: enable=broad-except
 
-    @classmethod
-    def _prepare_matches(cls, matches, that_img_id, distance_score):
-        match_exists = False
-        for match in matches:
-            if match["that_img_id"] == that_img_id:
-                match_exists = True
-                match["distance_score"] = min(match["distance_score"],
-                                              distance_score)
-        if not match_exists:
-            matches.append({
-                "that_img_id": that_img_id,
-                "distance_score": distance_score
-            })
 
-    def _update_img_status(self, img_id, status=None, error_msg=None):
-        session = self.db.get_session()
-        update_fields = {}
-        if status:
-            update_fields['status'] = status
-        if error_msg:
-            update_fields['error_msg'] = error_msg
-        session.query(ImageStatus).filter(
-            ImageStatus.img_id == img_id).update(update_fields)
-        self.db.safe_commit(session)
+def _process_feature_mapping(features, img_id, session):
+    logger.debug('processing feature mapping')
+    feature_str = json.dumps(features)
+    _add_entry_to_session(FeatureMapping, session,
+                          img_id=img_id, features=feature_str)
+    return features
 
-    def _img_should_be_processed(self, img_id):
-        session = self.db.get_session()
-        img_status = session.query(ImageStatus).filter(
-            ImageStatus.img_id == img_id).first()
-        session.close()
-        if img_status is not None:
-            return img_status.status == ImageStatusEnum.on_queue.name
+
+def _process_matches(this_img_id, that_img_id, distance_score, session):
+    logger.debug('processing matches')
+    _add_entry_to_session(Match, session,
+                          this_img_id=this_img_id,
+                          that_img_id=that_img_id,
+                          distance_score=distance_score)
+    _add_entry_to_session(Match, session,
+                          this_img_id=that_img_id,
+                          that_img_id=this_img_id,
+                          distance_score=distance_score)
+
+
+def _get_img_ids_and_features():
+    logger.debug('getting all img ids and respective features')
+    session = db.get_session()
+    known_features = []
+    rows = session.query(FeatureMapping).all()
+    session.close()
+    img_ids = []
+    for row in rows:
+        img_ids.append(row.img_id)
+        current_features = np.array(json.loads(row.features))
+        known_features.append(current_features)
+    return img_ids, np.array(known_features)
+
+
+def _prepare_matches(matches, that_img_id, distance_score):
+    match_exists = False
+    for match in matches:
+        if match["that_img_id"] == that_img_id:
+            match_exists = True
+            match["distance_score"] = min(match["distance_score"],
+                                          distance_score)
+    if not match_exists:
+        matches.append({
+            "that_img_id": that_img_id,
+            "distance_score": distance_score
+        })
+
+
+def _update_img_status(img_id, status=None, error_msg=None):
+    session = db.get_session()
+    update_fields = {}
+    if status:
+        update_fields['status'] = status.name
+    if error_msg:
+        update_fields['error_msg'] = error_msg
+    session.query(ImageStatus).filter(
+        ImageStatus.img_id == img_id).update(update_fields)
+    db.safe_commit(session)
+
+
+def _img_should_be_processed(img_id):
+    session = db.get_session()
+    img_status = session.query(ImageStatus).filter(
+        ImageStatus.img_id == img_id).first()
+    session.close()
+    if img_status is None:
         return False
+    return img_status.status == ImageStatusEnum.on_queue.name
 
-    # pylint: disable=len-as-condition
-    @classmethod
-    def _compute_distances(cls, face_encodings, face_to_compare):
-        if len(face_encodings) == 0:
-            return np.empty((0))
 
-        face_to_compare = np.array(face_to_compare)
-        return np.linalg.norm(face_encodings - face_to_compare, axis=1)
+# pylint: disable=len-as-condition
+def _compute_distances(face_encodings, face_to_compare):
+    if len(face_encodings) == 0:
+        return np.empty((0))
 
-    # pylint: disable=too-many-locals
-    def _handle_message_from_queue(self, message):
-        self.logger.debug("handling message from queue")
-        session = self.db.get_session()
-        curr_img_id = message.content
-        if not self._img_should_be_processed(curr_img_id):
-            session.close()
-            return
-        self._update_img_status(
-            curr_img_id, status=ImageStatusEnum.processing.name)
-        curr_img_path = self._find_image(curr_img_id, session)
-        if curr_img_path is not None:
-            prev_img_ids, prev_features = self._get_img_ids_and_features()
-            curr_matches = []
-            face_vectors = get_face_vectors(
-                curr_img_path, FACE_VECTORIZE_ALGORITHM)
-            if not face_vectors:
-                error_msg = "No faces found in image"
-                self._update_img_status(curr_img_id, error_msg=error_msg)
-            for face_vector in face_vectors:
-                self._process_feature_mapping(
-                    face_vector, curr_img_id, session)
-                face_distances = self._compute_distances(
-                    prev_features, face_vector)
-                for i, distance_score in enumerate(face_distances):
-                    that_img_id = prev_img_ids[i]
-                    if curr_img_id == that_img_id:
-                        continue
-                    distance_score = float(distance_score)
-                    if distance_score >= DISTANCE_SCORE_THRESHOLD:
-                        continue
-                    self._prepare_matches(
-                        curr_matches, that_img_id, distance_score)
-            for curr_match in curr_matches:
-                self._process_matches(
-                    curr_img_id, curr_match["that_img_id"],
-                    curr_match["distance_score"], session)
-        else:
-            error_msg = "Image processed before uploaded"
-            self._update_img_status(curr_img_id, error_msg=error_msg)
-        self._update_img_status(
-            curr_img_id, status=ImageStatusEnum.finished_processing.name)
-        self.db.safe_commit(session)
-        self._delete_img(curr_img_id)
+    face_to_compare = np.array(face_to_compare)
+    return np.linalg.norm(face_encodings - face_to_compare, axis=1)
+# pylint: enable=len-as-condition
 
-    def begin_pipeline(self):
-        self.logger.debug('pipeline began')
-        qp = QueuePoll(IMAGE_PROCESSOR_QUEUE)
-        for message in qp.poll():
-            self._handle_message_from_queue(message)
-            self.logger.debug("polling next iteration")
 
-# pylint: enable=too-few-public-methods
+# pylint: disable=too-many-locals
+def _handle_message_from_queue(message):
+    logger.debug("handling message from queue")
+    session = db.get_session()
+    curr_img_id = message.content
+    if not _img_should_be_processed(curr_img_id):
+        session.close()
+        return
+    _update_img_status(curr_img_id, status=ImageStatusEnum.processing)
+    curr_img_path = _find_image(curr_img_id, session)
+    if curr_img_path is not None:
+        prev_img_ids, prev_features = _get_img_ids_and_features()
+        curr_matches = []
+        face_vectors = get_face_vectors(
+            curr_img_path, FACE_VECTORIZE_ALGORITHM)
+        if not face_vectors:
+            error_msg = "No faces found in image"
+            _update_img_status(curr_img_id, error_msg=error_msg)
+        for face_vector in face_vectors:
+            _process_feature_mapping(face_vector, curr_img_id, session)
+            face_distances = _compute_distances(prev_features, face_vector)
+            for i, distance_score in enumerate(face_distances):
+                that_img_id = prev_img_ids[i]
+                if curr_img_id == that_img_id:
+                    continue
+                distance_score = float(distance_score)
+                if distance_score >= DISTANCE_SCORE_THRESHOLD:
+                    continue
+                _prepare_matches(curr_matches, that_img_id, distance_score)
+        for curr_match in curr_matches:
+            _process_matches(curr_img_id, curr_match["that_img_id"],
+                             curr_match["distance_score"], session)
+    else:
+        error_msg = "Image processed before uploaded"
+        _update_img_status(curr_img_id, error_msg=error_msg)
+    _update_img_status(curr_img_id, status=ImageStatusEnum.finished_processing)
+    db.safe_commit(session)
+    _delete_img(curr_img_id)
+# pylint: enable=too-many-locals
+
+
+def begin_pipeline():
+    logger.debug('pipeline began')
+    qp = QueuePoll(IMAGE_PROCESSOR_QUEUE)
+    for message in qp.poll():
+        _handle_message_from_queue(message)
+        logger.debug("polling next iteration")
--- app/worker.py
+++ app/worker.py
@@ -1,10 +1,9 @@
 from faceanalysis.models.database_manager import get_database_manager
 from faceanalysis.models.models import init_models
-from faceanalysis.pipeline import Pipeline
+from faceanalysis.pipeline import begin_pipeline
 
 if __name__ == "__main__":
     db = get_database_manager()
     init_models(db.engine)
-    pipeline = Pipeline()
-    pipeline.begin_pipeline()
+    begin_pipeline()
     db.close_engine()
