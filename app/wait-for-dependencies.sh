6aa10f72cfa55b678fb308e91e4ae442ebfd9c2d
Clemens Wolff <clewolff@microsoft.com>
2018-08-09 09:44:39 -0400

    Avoid unauthorized user warnings in MySQL logs

--- app/wait-for-dependencies.sh
+++ app/wait-for-dependencies.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-while ! mysqladmin ping -h"$MYSQL_HOST" --silent; do
-  sleep 1
-done
+mysql_is_available() {
+  mysqladmin ping -h"$MYSQL_HOST" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --silent > /dev/null
+}
+
+until mysql_is_available; do echo "Waiting for MySQL"; sleep 3; done
+echo "MySQL is alive"
 
 exec "$@"
