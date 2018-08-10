1f9a26be9b00642df6dacc6312a647858e4a9d15
Arman Rahman <arrahm@microsoft.com>
2018-08-10 02:56:31 -0500

    fixing comments from pull request

--- scripts/get_famous_people_photos/src/get_famous_people_photos.py
+++ scripts/get_famous_people_photos/src/get_famous_people_photos.py
@@ -1,5 +1,6 @@
 import collections
 import glob
+import gzip
 import itertools
 import os
 import pickle
@@ -12,23 +13,24 @@ from multiprocessing import Lock, Manager, Pool, Queue, Value
 from multiprocessing.dummy import Pool as ThreadPool
 from os import getenv
 from time import time
+from typing import Generator, List, Set
 
 import cv2
 import dhash
+import numpy as np
 import progressbar as pb
 import pybktree
 import requests
 import tensorflow as tf
+from facenet_sandberg import face
 from pathos.multiprocessing import ProcessPool
 from PIL import Image
 from requests import exceptions
-from facenet_sandberg import face
-
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-BING_API_KEY = str(getenv('BING_API_KEY', ''))
+BING_API_KEY = str(getenv('BING_API_KEY', 'c8e183c6cf57419bb0c0ee885b76bbe5'))
 NUM_THREADS = int(getenv('NUM_THREADS', 50))
-NUM_PROCESSES = min(int(getenv('NUM_PROCESSES', 6)), os.cpu_count())
+NUM_PROCESSES = min(int(getenv('NUM_PROCESSES', 4)), os.cpu_count())
 MAX_RESULTS = 150
 
 URL = "https://api.cognitive.microsoft.com/bing/v7.0/images/search"
@@ -36,18 +38,18 @@ HEADERS = {"Ocp-Apim-Subscription-Key": BING_API_KEY}
 
 IMAGE_HASH = collections.namedtuple('IMAGE_HASH', 'bits url')
 
-timer = None
-counter = Value(c_int)  # defaults to 0
-counter_lock = Lock()
+timer: pb.ProgressBar = None
+counter: Value = Value(c_int)  # defaults to 0
+counter_lock: Lock = Lock()
 
 
 facenet_model_checkpoint = "../../common/models/20180402-114759.pb"
-famous_people_file = '../../common/text_files/famous_people.txt'
+famous_people_file = '../../common/text_files/famous_people.txt.gz'
 url_file = '../../common/text_files/image_urls.data'
 images_directory = '../../common/images/'
 
 
-def increment(add_amount=1):
+def increment(add_amount: int=1):
     with counter_lock:
         counter.value += add_amount
 
@@ -59,11 +61,10 @@ def reset():
     timer = None
 
 
-def get_photos(famous_people_file):
-    people = []
-    with open(famous_people_file) as fp:
-        people = fp.read().splitlines()
-    unique_people = set(people)
+def get_photos(famous_people_file: str):
+    unique_people = set()
+    with gzip.open(famous_people_file, mode='rt', encoding='utf-8') as fp:
+        unique_people = {line.strip() for line in fp}
     total_count = len(unique_people)
 
     urls, people = fetch_urls_multithread(unique_people, total_count)
@@ -79,10 +80,10 @@ def get_photos(famous_people_file):
 
     multiprocess(download_urls, deduped_urls, people,
                  total_count, 'downloading images')
-    print('[INFO] Done getting photos.')
+    timer.finish()
 
 
-def fetch_urls_multithread(unique_people, total_count):
+def fetch_urls_multithread(unique_people: Set[str], total_count: int) -> (List[List[str]], List[str]):
     print("[INFO] Fetching image urls with {} threads".format(NUM_THREADS))
     global timer
     widgets_urls = ['Fetching urls: ', pb.Percentage(), ' ',
@@ -107,29 +108,35 @@ def fetch_urls_multithread(unique_people, total_count):
     return urls, people
 
 
-def multiprocess(function, urls, people, total_count, info):
+def multiprocess(function: any, all_urls: List[List[str]], people: List[str], total_count: int, info: str) -> (List[List[str]], List[str]):
     print("[INFO] {} with {} processes".format(info, NUM_PROCESSES))
     global timer
     widgets_match = ['{}: '.format(info), pb.Percentage(), ' ',
                      pb.Bar(marker=pb.RotatingMarker()), ' ', pb.ETA()]
     timer = pb.ProgressBar(widgets=widgets_match, maxval=total_count).start()
 
-    process_pool = ProcessPool(NUM_PROCESSES)
-    urls_and_people = process_pool.imap(function, urls, people)
-    process_pool.close()
-    process_pool.join()
-    filtered_urls, people = zip(*urls_and_people)
+    if NUM_PROCESSES > 1:
+        process_pool = ProcessPool(NUM_PROCESSES)
+        urls_and_people = process_pool.imap(function, all_urls, people)
+        process_pool.close()
+        process_pool.join()
+        filtered_urls, people = zip(*urls_and_people)
+    else:
+        filtered_urls = []
+        for urls, person in zip(all_urls, people):
+            filtered, person = function(urls, person)
+            filtered_urls.append(filtered)
     print("[INFO] Done {}".format(info))
     return filtered_urls, people
 
 
-def get_urls(person):
+def get_urls(person: str) -> (List[str], str):
     params = {"q": person, "count": MAX_RESULTS, "imageType": "photo"}
     search = requests.get(URL, headers=HEADERS, params=params)
     search.raise_for_status()
     results = search.json()
     thumbnail_urls = [img["thumbnailUrl"] +
-                      '&c=7&w=250&h=250' for img in results["value"]]
+                    '&c=7&w=250&h=250' for img in results["value"]]
     # update counter
     increment()
     timer.update(int(counter.value))
@@ -143,7 +150,7 @@ def safe_match_images(*args, **kwargs):
         print(e)
 
 
-def match_images(thumbnail_urls, person):
+def match_images(thumbnail_urls: List[str], person: str) -> (List[str], str):
     urls = set()
     directory = images_directory + person
     # Skip directories that have already been made
@@ -153,12 +160,13 @@ def match_images(thumbnail_urls, person):
             identifier = face.Identifier(
                 threshold=1.0, facenet_model_checkpoint=facenet_model_checkpoint)
             images = map(identifier.download_image, thumbnail_urls)
-            urls_and_embeddings = identifier.detect_encode_all(
-                images, thumbnail_urls, True)
-            anchor_embedding = list(islice(urls_and_embeddings, 0))[
-                0].embedding
+            all_faces: Generator[List[face.Face], None, None] = identifier.detect_encode_all(images, urls=thumbnail_urls, save_memory=True)
+            # Flattens the lists of faces into one generator 
+            faces = (face for faces in all_faces for face in faces)
+            # import pdb;pdb.set_trace()
+            anchor_embedding = list(islice(faces, 1))[0].embedding
             # Assume first image is of the right person and check other images are of the same person
-            for other in urls_and_embeddings:
+            for other in faces:
                 is_match, distance = identifier.compare_embedding(
                     anchor_embedding, other.embedding)
                 if is_match:
@@ -173,7 +181,7 @@ def match_images(thumbnail_urls, person):
     return list(urls), person
 
 
-def dedupe_images(matched_urls, person):
+def dedupe_images(matched_urls: List[str], person: str) -> (List[str], str):
     """Hashes images and removes nearly identical images
 
     Arguments:
@@ -192,8 +200,8 @@ def dedupe_images(matched_urls, person):
     urls_to_keep = set()
     for image_hash in sorted_image_hashes:
         if image_hash not in to_discard:
-            # gets pictures within a hamming distance of 3
-            matches = tree.find(image_hash, 3)
+            # gets pictures within a hamming distance of 5
+            matches = tree.find(image_hash, 5)
             for match in matches:
                 if match[1].url != image_hash.url:
                     to_discard.append(match[1])
@@ -201,10 +209,10 @@ def dedupe_images(matched_urls, person):
     # Update counter
     increment()
     timer.update(int(counter.value))
-    return list(urls_to_keep)
+    return list(urls_to_keep), person
 
 
-def download_urls(deduped_urls, person):
+def download_urls(deduped_urls: List[str], person: str):
     """Downloads the urls to the image directory
 
     Arguments:
@@ -216,8 +224,7 @@ def download_urls(deduped_urls, person):
 
     if len(deduped_urls) > 5:
         directory = images_directory + person
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        os.makedirs(directory, exist_ok=True)
         for count, url in enumerate(deduped_urls):
             image = url_to_image(url)
             image_path = os.path.join(directory, str(count))
@@ -228,7 +235,7 @@ def download_urls(deduped_urls, person):
     return None, None
 
 
-def url_to_image(url):
+def url_to_image(url: str) -> np.ndarray:
     """Converts a url to a PIL image
 
     Arguments:
@@ -244,7 +251,7 @@ def url_to_image(url):
     return image
 
 
-def url_to_img_hash(url):
+def url_to_img_hash(url: str) -> int:
     """Converts a url to an image hash
     
     Arguments:
@@ -259,7 +266,7 @@ def url_to_img_hash(url):
     return image_hash
 
 
-def image_distance(x, y):
+def image_distance(x: IMAGE_HASH, y: IMAGE_HASH) -> int:
     """Calculates the distance between to image hashes
     
     Arguments:
