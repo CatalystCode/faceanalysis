33a1960f2b2bb4b4ea7b07f23b710db1fd772328
Arman Rahman <arrahm@microsoft.com>
2018-08-11 17:40:17 -0500

    addressed pull request changes

--- algorithms/facenet/vectorize.py
+++ algorithms/facenet/vectorize.py
@@ -6,16 +6,18 @@ import numpy as np
 import tensorflow as tf
 from facenet_sandberg import face
 
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 tf.logging.set_verbosity(tf.logging.ERROR)
 
+FaceVector = List[float]
+Image = np.array
 
-def get_face_vectors(img_path: str) -> List[List[float]]:
-    identifier = identifier = face.Identifier(
+
+def get_face_vectors(img_path: str, prealigned: bool) -> List[List[float]]:
+    identifier = face.Identifier(
         facenet_model_checkpoint='20180402-114759.pb')
-    image: np.ndarray = identifier.get_image_from_path(img_path)
-    vectors: List[np.ndarray] = identifier.vectorize(image)
+    image = identifier.get_image_from_path(img_path)
+    vectors = identifier.vectorize(image, prealigned=prealigned)
     return [vector.tolist() for vector in vectors]
 
 
@@ -32,9 +34,11 @@ def _cli():
         image.close()
         image_paths.append(image.name)
 
+    prealigned = getenv('PREALIGNED') == 'true'
+
     # naive implementation for demo purposes, could also batch process images
-    vectors = {image_path: get_face_vectors(image_path)
-               for image_path in image_paths}
+    vectors = [get_face_vectors(image_path, prealigned)
+               for image_path in image_paths]
 
     print(json.dumps({'faceVectors': vectors}))
 
