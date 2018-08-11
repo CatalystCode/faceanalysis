1cd59167e666847872a6c7d1e4a490d83e392db2
Clemens Wolff <clewolff@microsoft.com>
2018-08-10 20:16:59 -0400

    Factor out find_faces method

--- algorithms/face_recognition/vectorize.py
+++ algorithms/face_recognition/vectorize.py
@@ -1,23 +1,45 @@
-import json
+from typing import Iterable
+from typing import List
+from typing import Optional
+
 import face_recognition as fr
+import numpy as np
+
+FaceVector = List[float]
+Image = np.array
 
 
-def get_face_vectors(img_path):
-    img = fr.load_image_file(img_path)
+def get_face_embedding(face: Image) -> Optional[FaceVector]:
+    cropped_features = fr.face_encodings(face)
+    if not cropped_features:
+        return None
+
+    face_vector = cropped_features[0]
+    return face_vector.tolist()
+
+
+def find_faces(img: Image) -> Iterable[Image]:
     face_locations = fr.face_locations(img)
-    face_vectors = []
     for top, right, bottom, left in face_locations:
-        cropped_img = img[top:bottom, left:right]
-        cropped_features = fr.face_encodings(cropped_img)
-        if cropped_features:
-            face_vector = cropped_features[0]
-            face_vectors.append(face_vector.tolist())
+        yield img[top:bottom, left:right]
+
+
+def get_face_vectors(img_path: str) -> List[FaceVector]:
+    img = fr.load_image_file(img_path)
+    faces = find_faces(img)
+
+    face_vectors = []
+    for face in faces:
+        face_vector = get_face_embedding(face)
+        if face_vector:
+            face_vectors.append(face_vector)
     return face_vectors
 
 
 def _cli():
     from argparse import ArgumentParser
     from argparse import FileType
+    import json
 
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('images', type=FileType('r'), nargs='+')
@@ -29,7 +51,8 @@ def _cli():
         image_paths.append(image.name)
 
     # naive implementation for demo purposes, could also batch process images
-    vectors = [get_face_vectors(image_path) for image_path in image_paths]
+    vectors = [get_face_vectors(image_path)
+               for image_path in image_paths]
 
     print(json.dumps({'faceVectors': vectors}))
 
