ae208ac56b0a332a03c67fe2a32bbd85a76461ee
Jason Fox <jason.fox@microsoft.com>
2018-08-16 11:28:43 -0500

    implemented PR review changes
    -moved to scripts directory
    -changed from opencv to scikit-image adaptive histogam equalization
    -changed getopt to argparse

--- scripts/preprocessor/processor.py
+++ scripts/preprocessor/processor.py
@@ -1,14 +1,17 @@
+from skimage import exposure, io
 from pathlib import Path
-import os
-import glob
-import cv2 as cv
 
 allowed_file_extensions = ['.png', '.jpg', '.jpeg']
 
-# recursively load and normalize images using histogram equalization from OpenCV
+
 def normalize_images(inputdirectory, outputdirectory):
     inputpath = Path(inputdirectory)
     outputpath = Path(outputdirectory)
+
+    if not inputpath.exists() or not outputpath.exists():
+        print('bad input or output path')
+        exit(2)
+
     inputfiles = []
 
     for fe in allowed_file_extensions:
@@ -18,25 +21,22 @@ def normalize_images(inputdirectory, outputdirectory):
         abs_file_path = Path.absolute(f)
         file_path = str(abs_file_path)
         output_file_name = get_output_file_name(f, inputpath, outputpath)
-        print('normalizing image: ' + str(f) + ' to: ' + str(output_file_name))
+        print('normalizing image: {} to: {}'.format(f, output_file_name))
 
         img_output = normalize_image(file_path)
-
         ensure_dir_exists_for_file(output_file_name)
-
         save_image(str(output_file_name), img_output)
 
+
 def normalize_image(file_path):
-        img = cv.imread(file_path)
-        img_yuv = cv.cvtColor(img, cv.COLOR_BGR2YUV)
-
-        img_yuv[:,:,0] = cv.equalizeHist(img_yuv[:,:,0])
-
-        img_output = cv.cvtColor(img_yuv, cv.COLOR_YUV2BGR)
+        img = io.imread(file_path)
+        img_output = exposure.equalize_adapthist(img)
         return img_output
 
+
 def save_image(file_path, img):
-    cv.imwrite(file_path, img)
+    io.imsave(file_path, img)
+
 
 def get_output_file_name(f, inputpath, outputpath):
         parent_dir = f.parent
@@ -48,6 +48,7 @@ def get_output_file_name(f, inputpath, outputpath):
             output_file_name = outputpath.joinpath(f.name)
 
         return output_file_name
+
 
 def ensure_dir_exists_for_file(file_name):
         output_parent_dir = file_name.parent
