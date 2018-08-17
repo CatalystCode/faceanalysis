07162fa26fd249daa916f30b386b539c5fb94e51
Clemens Wolff <clewolff@microsoft.com>
2018-08-17 11:27:20 -0400

    Fix pylint warnings

--- scripts/preprocessor/main.py
+++ scripts/preprocessor/main.py
@@ -1,26 +1,36 @@
-import sys
-import argparse
 from processor import normalize_images
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--input',
-                    help='the path to the input directory',
-                    required=True)
-parser.add_argument('-o', '--output',
-                    help='the path to the output directory',
-                    required=True)
 
-
-def main(argv):
-    args = parser.parse_args()
-    inputdirectory = args.input
-    outputdirectory = args.output
-
+def _main(inputdirectory, outputdirectory):
     print('Input directory is {}.'.format(inputdirectory))
     print('Output directory is {}.'.format(outputdirectory))
 
     normalize_images(inputdirectory, outputdirectory)
 
 
+def _cli():
+    from argparse import ArgumentParser
+    from argparse import ArgumentTypeError
+    from os.path import isdir
+
+    def directory_type(arg):
+        if not isdir(arg):
+            raise ArgumentTypeError('{} is not a directory'.format(arg))
+        return arg
+
+    parser = ArgumentParser()
+    parser.add_argument('-i', '--input',
+                        type=directory_type,
+                        help='the path to the input directory',
+                        required=True)
+    parser.add_argument('-o', '--output',
+                        type=directory_type,
+                        help='the path to the output directory',
+                        required=True)
+    args = parser.parse_args()
+
+    _main(args.input, args.output)
+
+
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    _cli()
--- scripts/preprocessor/processor.py
+++ scripts/preprocessor/processor.py
@@ -1,5 +1,7 @@
-from skimage import exposure, io
 from pathlib import Path
+
+from skimage import exposure
+from skimage import io
 
 allowed_file_extensions = ['.png', '.jpg', '.jpeg']
 
@@ -8,20 +10,16 @@ def normalize_images(inputdirectory, outputdirectory):
     inputpath = Path(inputdirectory)
     outputpath = Path(outputdirectory)
 
-    if not inputpath.exists() or not outputpath.exists():
-        print('bad input or output path')
-        exit(2)
-
     inputfiles = []
 
-    for fe in allowed_file_extensions:
-        inputfiles.extend(list(inputpath.glob('**/*' + fe)))
+    for extension in allowed_file_extensions:
+        inputfiles.extend(list(inputpath.glob('**/*' + extension)))
 
-    for f in inputfiles:
-        abs_file_path = Path.absolute(f)
+    for infile in inputfiles:
+        abs_file_path = Path.absolute(infile)
         file_path = str(abs_file_path)
-        output_file_name = get_output_file_name(f, inputpath, outputpath)
-        print('normalizing image: {} to: {}'.format(f, output_file_name))
+        output_file_name = get_output_file_name(infile, inputpath, outputpath)
+        print('normalizing image: {} to: {}'.format(infile, output_file_name))
 
         img_output = normalize_image(file_path)
         ensure_dir_exists_for_file(output_file_name)
@@ -29,29 +27,29 @@ def normalize_images(inputdirectory, outputdirectory):
 
 
 def normalize_image(file_path):
-        img = io.imread(file_path)
-        img_output = exposure.equalize_adapthist(img)
-        return img_output
+    img = io.imread(file_path)
+    img_output = exposure.equalize_adapthist(img)
+    return img_output
 
 
 def save_image(file_path, img):
     io.imsave(file_path, img)
 
 
-def get_output_file_name(f, inputpath, outputpath):
-        parent_dir = f.parent
+def get_output_file_name(infile, inputpath, outputpath):
+    parent_dir = infile.parent
 
-        if not inputpath.samefile(Path(parent_dir)):
-            parent_dir_name = parent_dir.stem
-            output_file_name = outputpath.joinpath(parent_dir_name, f.name)
-        else:
-            output_file_name = outputpath.joinpath(f.name)
+    if not inputpath.samefile(Path(parent_dir)):
+        parent_dir_name = parent_dir.stem
+        output_file_name = outputpath.joinpath(parent_dir_name, infile.name)
+    else:
+        output_file_name = outputpath.joinpath(infile.name)
 
-        return output_file_name
+    return output_file_name
 
 
 def ensure_dir_exists_for_file(file_name):
-        output_parent_dir = file_name.parent
+    output_parent_dir = file_name.parent
 
-        if not Path.exists(output_parent_dir):
-            Path.mkdir(output_parent_dir)
+    if not Path.exists(output_parent_dir):
+        Path.mkdir(output_parent_dir)
