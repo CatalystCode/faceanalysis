e546db9f4c4e594f90daca485a6d276e9ba3e791
Shawn Cicoria <github@cicoria.com>
2018-11-05 15:56:40 -0500

    adding image component

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+// import logo from './logo.svg';
 
-import ImageViewer from './components/ImageViewer';
+import { ImageViewer } from './components/ImageViewer';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2 className="App-title">Welcome to React</h2>
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <h2 className="App-title">Welcome to React</h2> */}
         </header>
-        <p className="App-intro">
-          <ImageViewer/>
-        </p>
-
+        <div className="App-intro">
+          <ImageViewer url={"https://images.pexels.com/photos/736716/pexels-photo-736716.jpeg"} 
+            height={200} width={300} />
+        </div>
       </div>
     );
   }
