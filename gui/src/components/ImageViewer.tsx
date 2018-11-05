42fafb586d410b68a7fc5b380f0be5884e5ad95f
Shawn Cicoria <github@cicoria.com>
2018-11-05 15:56:40 -0500

    adding image component

--- gui/src/components/ImageViewer.tsx
+++ gui/src/components/ImageViewer.tsx
@@ -1,7 +1,25 @@
 import * as React from 'react';
-class ImageViewer extends React.Component {
+// import { Image } from 'reactstrap';
+// import { Image } from 'react-dom'
+import './ImageViewer.css';
 
-
+export interface Props {
+  url: string;
+  alt?: string;
+  width: number;
+  height: number
 }
 
-export default ImageViewer;
+export const ImageViewer = (props: Props) => {
+  const img = new Image(props.height, props.width);
+  img.src = props.url;
+  
+
+  return (
+    <div className="img-right">
+      <img src={img.src} height={img.height} width={img.width} />
+    </div>
+  )
+}
+
+
