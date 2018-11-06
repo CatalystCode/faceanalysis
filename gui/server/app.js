22e4ea63bd5be5619a74197e4685c745f6c4f35e
Jason Fox <jason.fox@microsoft.com>
2018-11-06 11:46:14 +0100

    added routes for metadata and a mock metadata provider

--- gui/server/app.js
+++ gui/server/app.js
@@ -6,8 +6,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var metadataRouter = require('./routes/metadata');
 
 var app = express();
+const port = 3001;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/metadata', metadataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +40,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app;
--- gui/server/routes/metadata.js
+++ gui/server/routes/metadata.js
@@ -1,21 +1,29 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+const provider = require('../providers/metadataprovider');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/meta', function(req, res, next) {
-  const metadata = fs.readFileSync('../');
+const metadataFile = './gui/server/data/ttf-meta.json';
 
-
+/* GET all case metadata. */
+router.get('/', function(req, res, next) {
+  provider.getmetadata((err, metatdata) => {
+    if( err ) {
+    } else {
+      console.log(metatdata);
+    }
+  })
 });
 
-/* GET home page. */
-router.get('/meta/:id', function(req, res, next) {
-  const metadata = fs.readFileSync('../');
-
-
+/* GET case metadata by id. */
+router.get('/:id', function(req, res, next) {
+  provider.getmetadatabyid(req.params.id, (err, metatdata) => {
+    if( err ) {
+    } else {
+      console.log(metatdata);
+    }
+  })
 });
 
 module.exports = router;
