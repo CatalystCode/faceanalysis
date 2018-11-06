f2e4e97472bd7b97dd16cec42a94289f616329c0
Shawn Cicoria <github@cicoria.com>
2018-11-06 04:55:29 -0500

    run ALL from npm run all`

--- gui/server/app.js
+++ gui/server/app.js
@@ -6,8 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var metadataRouter = require('./routes/metadata');
 
 var app = express();
+
+// this is the base directory where app.js - can't relay on __dirname 
+// as it's relative.
+global.__basedir = __dirname;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', metadataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
