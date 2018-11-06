d1a95cedcdce0219972fa5080335dba828e15fe2
Clemens Wolff <clewolff@microsoft.com>
2018-11-06 10:14:25 +0100

    Remove unnecessary dollar sign

--- algorithms/FaceApi/FaceIdentifier.cs
+++ algorithms/FaceApi/FaceIdentifier.cs
@@ -80,7 +80,7 @@ namespace FaceApi
                         return true;
 
                     case TrainingStatusType.Failed:
-                        await Console.Error.WriteLineAsync($"Training of model {groupId} failed: ${status.Message}");
+                        await Console.Error.WriteLineAsync($"Training of model {groupId} failed: {status.Message}");
                         return false;
                 }
             }
