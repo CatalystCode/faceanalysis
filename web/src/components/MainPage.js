0e1032bf81d3a12b8ae5a178937bcdfade734217
Shawn Cicoria <github@cicoria.com>
2018-11-07 12:41:09 -0500

    add local json simple server

--- web/src/components/MainPage.js
+++ web/src/components/MainPage.js
@@ -5,35 +5,40 @@ import PropTypes from 'prop-types';
 import { getImages } from '../redux/actions/images';
 
 class MainPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          appData : {}
-        };
-        // this.handler = this.handler.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      subjectImage: {},
+      targetImage : {},
+      candidateImages: [],
+      hasErrored: false,
+      isLoading: false,
+      appData: {}
+    };
+    // this.handler = this.handler.bind(this);
+  }
 
-    componentDidMount(){
-      // fetch the data...
-      this.props.getImages();
-    }
+  componentDidMount() {
+    // fetch the data...
+    this.props.getImages();
+  }
 
-    handler(val) {
-        // this.props.action();
-    }
+  handler(val) {
+    // this.props.action();
+  }
 
-    render() {
-        return (
-            <div>
-                <div>TBD</div>
-                {/* <PageTop>
+  render() {
+    return (
+      <div>
+        <div>TBD</div>
+        {/* <PageTop>
                     <--Menu />
                 </PageTop> */}
-                {/* <Routes  action={this.handler}  /> */}
-                {/* <Footer /> */}
-            </div>
-        );
-    }
+        {/* <Routes  action={this.handler}  /> */}
+        {/* <Footer /> */}
+      </div>
+    );
+  }
 }
 
 MainPage.defaultProps = {};
@@ -41,17 +46,17 @@ MainPage.propTypes = {};
 
 const mapStateToProps = (state) => {
   return {
-      subjecImage: state.images.data, // object of meta and URI
-    //   targetImage: state.images.targetImage.data, // object of meta and URI
-    //   candidateImages: state.images.candidateImages.data, // an array
-      hasErrored: state.images.hasErrored,
-      isLoading: state.images.isLoading,
-      errorMessage: state.images.errorMessage
+    subjectImage: state.images.data, // object of meta and URI
+    // targetImage: state.images.targetImage.data, // object of meta and URI
+    // candidateImages: state.images.candidateImages.data, // an array
+    hasErrored: state.images.hasErrored,
+    isLoading: state.images.isLoading,
+    errorMessage: state.images.errorMessage
   };
 };
 
 export default connect(mapStateToProps,
-    { getImages} )(MainPage);
+  { getImages })(MainPage);
 
 // export default MainPage;
 
